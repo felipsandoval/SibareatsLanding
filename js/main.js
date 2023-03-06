29dc756c01ae93a2dfa65e90bfb19509de08638b
Felipe Enmanuel Sandoval Sibada <felipesandoval1111@gmail.com>
2023-03-06 21:07:59 +0100

    updating

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var bubblyButtons = document.getElementById("bubbly-button");
+bubblyButtons.addEventListener('click', animateButton)
+
+function animateButton(e) {
+  e.preventDefault;
+  //reset animation
+  e.target.classList.remove('animate');
+  
+  e.target.classList.add('animate');
+  setTimeout(function(){
+    e.target.classList.remove('animate');
+  },700);
+};
+
+function OpenRouteSanchi(){
+    window.open("https://goo.gl/maps/uSz94Q75HmPL1mwu6")
+}
